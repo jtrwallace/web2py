fcd27dcfc2ecc0f0276439650a29fff479537af8
Joshua Wallace <jotwalla@ucsc.edu>
2015-10-09 11:11:19 -0700

    First and second reviews.

---
+++ applications/firstReview/models/icon.py
@@ -0,0 +1,3 @@
+icon_star_yellow = I(_class= 'fa fa-star yellow')
+
+icon_star_grey = I(_class= 'fa fa-star grey')
---
+++ applications/secondReview/models/icons.py
@@ -0,0 +1,2 @@
+icon_starred = I(_class='fa fa-star')
+icon_not_starred = I(_class='fa fa-star-o')
